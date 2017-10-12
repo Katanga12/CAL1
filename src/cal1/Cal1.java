71779214ab62c93ca86f50cb3ea8264deb526122
Student <Student@ZAPRE-D00000157.DirectDelivery.com>
2017-10-12 10:53:07 +0200

    initial project

---
+++ src/cal1/Cal1.java
@@ -0,0 +1,19 @@
+
+package cal1;
+
+/**
+ *
+ * @author Student
+ */
+public class Cal1 {
+    public int Add(String S1, String S2){
+        return 0;
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
